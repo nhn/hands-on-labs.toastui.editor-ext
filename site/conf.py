e3d296822e4775b182637a5ebaff9d2891ced47f
abraxas73 <abraxas73@gmail.com>
2020-11-15 15:35:21 +0900

    Update conf.py
    should use unicode

--- site/conf.py
+++ site/conf.py
@@ -57,7 +57,7 @@ source_encoding = 'utf-8'
 master_doc = 'index'
 
 # General information about the project.
-project = 'TOAST UI Editor 유튜브 플러그인 개발하기'
+project = u'TOAST UI Editor 유튜브 플러그인 개발하기'
 copyright = 'NHN Corp. All rights reserved.'
 author = ''
 
